9ba5e6cdd5d93d02020cffeaa4ee189073557a01
Giorgio Sironi <g.sironi@elifesciences.org>
2023-02-06 16:58:52 +0000

    Write a todo test

--- test/hello-world.test.ts
+++ test/hello-world.test.ts
@@ -2,3 +2,7 @@ test('adds 1 + 2 to equal 3', () => {
   const result: number = 1 + 2;
   expect(result).toBe(3);
 });
+
+describe('when there are no items', () => {
+  it.todo('the price is 0');
+})
