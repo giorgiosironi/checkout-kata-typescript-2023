49443cb87afdd1e42439fb38a524f926fe60a504
Giorgio Sironi <work@giorgiosironi.com>
2025-05-20 11:43:28 +0100

    Install a web framework

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import { price } from "./price";
+import express from "express";
 
 console.log(`Hello world! The price is ${price('something')}`);
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+    console.log(`Hello world app listening on port 3000`)
+})
